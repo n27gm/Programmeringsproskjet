c105d18638c4684800603104f5bb9908eb134d7f
Thomas Aarheim Nilsen <thomas.aarheim@gmail.com>
2024-10-14 13:33:08 +0200

    Oppgave 1 ferdig

--- src/no/hvl/dat100ptc/oppgave1/GPSPoint.java
+++ src/no/hvl/dat100ptc/oppgave1/GPSPoint.java
@@ -4,73 +4,73 @@ import no.hvl.dat100ptc.TODO;
 
 public class GPSPoint {
 
-	// TODO - objektvariable
-	
+	private int time;
+	private double latitude;
+	private double longitude;
+	private double elevation;
+
 	public GPSPoint(int time, double latitude, double longitude, double elevation) {
 
-		// TODO - konstruktur
+		this.time = time;
+		this.latitude = latitude;
+		this.longitude = longitude;
+		this.elevation = elevation;
 
-		throw new UnsupportedOperationException(TODO.construtor("GPSPoint"));
-		
 	}
 
 	// TODO - get/set metoder
 	public int getTime() {
-		
-		throw new UnsupportedOperationException(TODO.method());
-		
+
+		return time;
+
 	}
 
 	public void setTime(int time) {
-				
-		throw new UnsupportedOperationException(TODO.method());
-		
+
+		this.time = time;
 	}
 
 	public double getLatitude() {
-		
-		throw new UnsupportedOperationException(TODO.method());
-		
-		
+
+		return latitude;
+
 	}
 
 	public void setLatitude(double latitude) {
-		
-		throw new UnsupportedOperationException(TODO.method());
-		
+
+		this.latitude = latitude;
+
 	}
 
 	public double getLongitude() {
-		
-		throw new UnsupportedOperationException(TODO.method());
-		
+
+		return longitude;
+
 	}
 
 	public void setLongitude(double longitude) {
-		
-		throw new UnsupportedOperationException(TODO.method());
-		
+
+		this.longitude = longitude;
+
 	}
 
 	public double getElevation() {
-		
-		throw new UnsupportedOperationException(TODO.method());
-		
+
+		return elevation;
+
 	}
 
 	public void setElevation(double elevation) {
-		
-		throw new UnsupportedOperationException(TODO.method());
-		
-	}
-	
-	public String toString() {
-		
-		String str;
-		
-		throw new UnsupportedOperationException(TODO.method());
 
-		// TODO
-		
+		this.elevation = elevation;
+
+	}
+
+	public String toString() {
+
+		String str;
+
+		return time + " (" + latitude + "," + longitude + ") " + elevation + "\n";
+
 	}
 }
--- src/no/hvl/dat100ptc/oppgave1/Main.java
+++ src/no/hvl/dat100ptc/oppgave1/Main.java
@@ -3,9 +3,12 @@ package no.hvl.dat100ptc.oppgave1;
 public class Main {
 
 	public static void main(String[] args) {
-		
-		// TODO
-		
-	}
 
+	     GPSPoint gpsPoint = new GPSPoint(1, 2.0, 3.0, 5.0);
+	    System.out.println(gpsPoint.getTime());
+	    gpsPoint.setTime(2); 
+	    System.out.println(gpsPoint.toString());
+	    
+	     	     
+	}
 }
