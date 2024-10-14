e74bc267eacfe35efbe12449586b991683651abf
184930 <184930@stud.hvl.no>
2024-10-14 21:04:53 +0200

    Oppgave 2 ferdig

--- src/no/hvl/dat100ptc/oppgave2/Main.java
+++ src/no/hvl/dat100ptc/oppgave2/Main.java
@@ -4,10 +4,18 @@ import no.hvl.dat100ptc.oppgave1.GPSPoint;
 
 public class Main {
 
-	
-	public static void main(String[] args) {
+    public static void main(String[] args) {
+        GPSPoint point1 = new GPSPoint(0, 1.0, 2.0, 3.0);
+        GPSPoint point2 = new GPSPoint(0, 4.0, 5.0, 6.0);
+        GPSPoint point3 = new GPSPoint(0, 7.0, 8.0, 9.0);
 
-		// TODO
-		
-	}
+        GPSData gpsData = new GPSData(3);
+
+        gpsData.insertGPS(point1);
+        gpsData.insertGPS(point2);
+        gpsData.insertGPS(point3);
+
+        gpsData.print();
+    }
+
 }
