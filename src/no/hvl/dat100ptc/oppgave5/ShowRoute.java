fdaef62ddd71ebdba2ccdad7f37e4a68c8ca38ce
184930 <184930@stud.hvl.no>
2024-10-20 19:44:37 +0200

    Oppgave 5 ferdig

--- src/no/hvl/dat100ptc/oppgave5/ShowRoute.java
+++ src/no/hvl/dat100ptc/oppgave5/ShowRoute.java
@@ -64,32 +64,62 @@ public class ShowRoute extends EasyGraphics {
 
 	public void showRouteMap(int ybase) {
 
-		// TODO 
-		throw new UnsupportedOperationException(TODO.method());
-		
+	    setColor(0, 0, 255); 
+
+	    for (int i = 0; i < gpspoints.length - 1; i++) {
+		    
+	        int x1 = MARGIN + (int) ((gpspoints[i].getLongitude() - minlon) * xstep);
+	        int y1 = ybase - (int) ((gpspoints[i].getLatitude() - minlat) * ystep);
+	        int x2 = MARGIN + (int) ((gpspoints[i + 1].getLongitude() - minlon) * xstep);
+	        int y2 = ybase - (int) ((gpspoints[i + 1].getLatitude() - minlat) * ystep);
+
+ 	        drawLine(x1, y1, x2, y2);
+	    }
 	}
+
 
 	public void showStatistics() {
 
-		int TEXTDISTANCE = 20;
+	    int TEXTDISTANCE = 20;  
+	    int yPosition = 20;    
 
-		setColor(0,0,0);
-		setFont("Courier",12);
-		
-		drawString("Total time : " + gpscomputer.totalTime() + "\n" + 
-		"Total distance : " + gpscomputer.totalDistance() + "km" + "\n" +
-		"Total elevation: " + gpscomputer.totalElevation() + "m" + "\n" +
-		"Max speed      : " + (gpscomputer.maxSpeed()*3.6) + "km/t" + "\n" +
-		"Average speed  : " + gpscomputer.averageSpeed()*3.6 + "km/t" + "\n" +
-		"Energy         : " + gpscomputer.totalKcal(80) + "kcal",TEXTDISTANCE,0);
-		
+	    setColor(0, 0, 0); 
+	    setFont("Courier", 12);  
+
+	    drawString("Total time: " + GPSUtils.formatTime(gpscomputer.totalTime()), TEXTDISTANCE, yPosition);
+	    yPosition += 20;
+	    drawString("Total distance: " + String.format("%.2f", gpscomputer.totalDistance() / 1000) + " km", TEXTDISTANCE, yPosition);
+	    yPosition += 20;
+	    drawString("Total elevation: " + String.format("%.2f", gpscomputer.totalElevation()) + " m", TEXTDISTANCE, yPosition);
+	    yPosition += 20;
+	    drawString("Max speed: " + String.format("%.2f", gpscomputer.maxSpeed() * 3.6) + " km/t", TEXTDISTANCE, yPosition);
+	    yPosition += 20;
+	    drawString("Average speed: " + String.format("%.2f", gpscomputer.averageSpeed() * 3.6) + " km/t", TEXTDISTANCE, yPosition);
+	    yPosition += 20;
+	    drawString("Energy: " + String.format("%.2f", gpscomputer.totalKcal(80)) + " kcal", TEXTDISTANCE, yPosition);
 	}
+
 
 	public void replayRoute(int ybase) {
 
-		// TODO 
-		throw new UnsupportedOperationException(TODO.method());
-		
+	    int x = MARGIN + (int) ((gpspoints[0].getLongitude() - minlon) * xstep);
+	    int y = ybase - (int) ((gpspoints[0].getLatitude() - minlat) * ystep);
+
+	    int radius = 5;
+	    int circle = fillCircle(x, y, radius);
+	    setColor(0, 0, 255); 
+
+	    for (int i = 1; i < gpspoints.length; i++) {
+	        int xNext = MARGIN + (int) ((gpspoints[i].getLongitude() - minlon) * xstep);
+	        int yNext = ybase - (int) ((gpspoints[i].getLatitude() - minlat) * ystep);
+
+	        setSpeed(10);  
+
+	        moveCircle(circle, xNext, yNext);
+
+	        pause(100);
+	    }
 	}
+
 
 }
